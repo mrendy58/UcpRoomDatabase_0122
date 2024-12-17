c91fdfe58d697b51f2496c0258ffca865ee9d3a5
mrendy58 <derendyyy58@gmail.com>
2024-12-17 23:47:56 +0700

    membuat RepositoryMatkul untuk mendefinisikan interface pada entitas matakuliah

---
+++ app/src/main/java/com/example/ucp2/repository/RepositoryMatkul.kt
@@ -0,0 +1,16 @@
+package com.example.ucp2.repository
+
+import com.example.ucp2.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryMatkul {
+    suspend fun insertMataKuliah(mataKuliah: MataKuliah)
+
+    fun getALLMataKuliah(): Flow<List<MataKuliah>>
+
+    fun getMataKuliah(kode : String): Flow<MataKuliah>
+
+    suspend fun deleteMataKuliah(mataKuliah: MataKuliah)
+
+    suspend fun updateMataKuliah(mataKuliah: MataKuliah)
+}
