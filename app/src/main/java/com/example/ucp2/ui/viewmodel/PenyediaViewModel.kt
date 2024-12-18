bd321345e2258f7dda18167b144dbada6f3992f1
mrendy58 <derendyyy58@gmail.com>
2024-12-18 23:09:11 +0700

    membuat objek PenyediaViewModel untuk menyediakan sebuah ViewModel Factory

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.ucp2.ui.viewmodel
+
+import android.text.Editable.Factory
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+
+    }
+}
