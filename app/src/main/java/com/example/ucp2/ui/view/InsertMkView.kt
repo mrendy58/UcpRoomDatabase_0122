326119c9ff934cdfbf7b78a77f03e9a66f7afc1b
rendy58 <name>
2024-12-19 13:41:22 +0700

    membuat insert body utuk mengatur inputan form

--- app/src/main/java/com/example/ucp2/ui/view/InsertMkView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/InsertMkView.kt
@@ -1,19 +1,28 @@
 package com.example.ucp2.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.customwidget.TopAppBar
 import com.example.ucp2.ui.viewmodel.DosenViewModel
 import com.example.ucp2.ui.viewmodel.InsertMkViewModel
+import com.example.ucp2.ui.viewmodel.MataKuliahEvent
+import com.example.ucp2.ui.viewmodel.MkUiState
 import kotlinx.coroutines.launch
 
 @Composable
@@ -69,6 +78,36 @@ fun InsertMkView(
                     onNavigate()
                 }
             )
+        }
+    }
+}
+
+// membuat insert body
+@Composable
+fun InsertBodyMk(
+    modifier: Modifier = Modifier,
+    uiState: MkUiState,
+    dosenList: List<String>,
+    onValueChange: (MataKuliahEvent) -> Unit,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormMataKuliah(
+            mataKuliahEvent = uiState.mataKuliahEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            dosenList = dosenList,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Simpan")
         }
     }
 }
