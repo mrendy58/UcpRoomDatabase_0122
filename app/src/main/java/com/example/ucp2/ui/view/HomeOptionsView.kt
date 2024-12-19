ff3dc35c8638dba5b17790837abb6055f1803253
rendy58 <name>
2024-12-19 12:27:14 +0700

    menambahkan update state pada button dan viewmodel

--- app/src/main/java/com/example/ucp2/ui/view/HomeOptionsView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/HomeOptionsView.kt
@@ -7,14 +7,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.ucp2.ui.viewmodel.HomeOptionsViewModel
 import com.example.ucp2.ui.customwidget.TopAppBar
-
-
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun HomeOptionsView(
     navController: NavController,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    homeOptionsViewModel: HomeOptionsViewModel = viewModel()
 ) {
     Scaffold(
         topBar = {
@@ -35,7 +36,11 @@ fun HomeOptionsView(
             ) {
                 // Tombol untuk Dosen
                 Button(
-                    onClick = { navController.navigate("listDosen") },
+                    onClick = {
+                        // Update state
+                        homeOptionsViewModel.updateMenuSelection("Dosen")
+                        navController.navigate("listDosen")
+                    },
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     Text("Dosen")
@@ -43,7 +48,11 @@ fun HomeOptionsView(
                 Spacer(modifier = Modifier.height(16.dp))
                 // Tombol untuk Mata Kuliah
                 Button(
-                    onClick = { navController.navigate("listMataKuliah") },
+                    onClick = {
+                        // Update state
+                        homeOptionsViewModel.updateMenuSelection("Mata Kuliah")
+                        navController.navigate("listMataKuliah")
+                    },
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     Text("Mata Kuliah")
