e46a19d0a7cc21efa97caf926a44994a1b2bb864
rendy58 <name>
2024-12-19 21:22:14 +0700

    membuat fungsi untuk mengambil daftar nama dosen

--- app/src/main/java/com/example/ucp2/repository/RepositoryMatkul.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryMatkul.kt
@@ -1,5 +1,5 @@
 package com.example.ucp2.repository
-
+import com.example.ucp2.data.dao.DosenDao
 import com.example.ucp2.data.entity.MataKuliah
 import kotlinx.coroutines.flow.Flow
 
@@ -8,9 +8,13 @@ interface RepositoryMatkul {
 
     fun getALLMataKuliah(): Flow<List<MataKuliah>>
 
-    fun getMataKuliah(kode : String): Flow<MataKuliah>
+    fun getMataKuliah(kode: String): Flow<MataKuliah>
 
     suspend fun deleteMataKuliah(mataKuliah: MataKuliah)
 
     suspend fun updateMataKuliah(mataKuliah: MataKuliah)
+
+    suspend fun getDosenList(dosenDao: DosenDao): List<String> {
+        return dosenDao.getAllDosen() // Panggil method dari instance dosenDao
+    }
 }
