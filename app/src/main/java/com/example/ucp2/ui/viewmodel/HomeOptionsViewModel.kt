9d1cbb0915e89ab9b7392208561c27cf62aa1d77
mrendy58 <derendyyy58@gmail.com>
2024-12-21 06:16:38 +0700

    memperbaiki home view model

--- app/src/main/java/com/example/ucp2/ui/viewmodel/HomeOptionsViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/HomeOptionsViewModel.kt
@@ -1,24 +1,56 @@
 package com.example.ucp2.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.repository.RepositoryDsn
+import com.example.ucp2.repository.RepositoryMatkul
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 // ViewModel untuk halaman Home
-class HomeOptionsViewModel : ViewModel() {
+class HomeOptionsViewModel (
+    private val repositoryDsn: RepositoryDsn,
+    private val repositoryMatkul: RepositoryMatkul
+): ViewModel() {
 
-    // State untuk menu utama
-    var uiState by mutableStateOf(HomeOptionsState())
-        private set
+    private val _uiState = MutableStateFlow(HomeOptionsState())
+    val uiState: StateFlow<HomeOptionsState> = _uiState
 
-    // Fungsi untuk menangani navigasi atau state lainnya (misalnya memilih menu)
-    fun updateMenuSelection(menu: String) {
-        uiState = uiState.copy(selectedMenu = menu)
+    init {
+        fetchData()
+    }
+
+    private fun fetchData() {
+        _uiState.value = _uiState.value.copy(isLoading = true)
+        viewModelScope.launch {
+            try {
+                val dosenlist = repositoryDsn.getALLDosen().first()
+                val mataKuliahList = repositoryMatkul.getALLMataKuliah().first()
+
+                _uiState.value = _uiState.value.copy(
+                    dosenList = dosenlist,
+                    mataKuliahList = mataKuliahList,
+                    isLoading = false
+                )
+            } catch (e: Exception) {
+                _uiState.value = _uiState.value.copy(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message ?: "Terjadi kesalahan saat memuat data"
+                )
+            }
+        }
     }
 }
-
 // Data class untuk state UI pada halaman Home
 data class HomeOptionsState(
-    val selectedMenu: String = ""
+    val dosenList: List<Dosen> = emptyList(),
+    val mataKuliahList: List<MataKuliah> = emptyList(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String? = null
 )
