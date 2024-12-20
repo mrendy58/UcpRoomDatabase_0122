6fe3f2da3101d2b526b900684cf73bc8c08ae4af
mrendy58 <derendyyy58@gmail.com>
2024-12-21 01:09:20 +0700

    membuat fungsi untuk menampilkan detail dengan judul

--- app/src/main/java/com/example/ucp2/ui/view/DetailMkView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/DetailMkView.kt
@@ -1,16 +1,52 @@
 package com.example.ucp2.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ucp2.data.entity.MataKuliah
 
 @Composable
 fun DetailMkView(){
 
 }
 
+@Composable
+fun PartDetailMk(
+    modifier: Modifier = Modifier,
+    judul: String,
+    data: String
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = data,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
 @Composable
 private fun DeleteConfDialog(
     onDeleteConfirm: () -> Unit,
