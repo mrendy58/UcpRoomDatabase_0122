bd2d01ecee1b4fe4d941785e6e0313971dfba267
rendy58 <name>
2024-12-20 15:45:51 +0700

    membuat ListDosenViewModel untuk menangani pengambilan data dari repository

--- app/src/main/java/com/example/ucp2/ui/viewmodel/ListDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/ListDosenViewModel.kt
@@ -1,7 +1,43 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.repository.RepositoryDsn
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+
+class ListDosenViewModel(
+    private val repositoryDsn: RepositoryDsn
+) : ViewModel() {
+    val dosenListUiState: StateFlow<DosenListUiState> = repositoryDsn.getALLDosen()
+        .filterNotNull()
+        .map {
+            DosenListUiState(
+                listDosen = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DosenListUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(DosenListUiState(
+                isLoading = false,
+                isError = true,
+                errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = DosenListUiState(isLoading = true)
+        )
+}
 data class DosenListUiState(
-    val listDosen: List<String> = listOf(),
+    val listDosen: List<Dosen> = listOf(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String = ""
