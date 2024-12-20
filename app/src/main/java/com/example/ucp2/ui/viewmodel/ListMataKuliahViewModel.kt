14db9257aaf0abbd313f0dc83ad73b0a20fa55df
rendy58 <name>
2024-12-20 21:29:26 +0700

    membuat ListMataKuliahViewModel untuk menangani pengambilan data dari repository matakuliah

--- app/src/main/java/com/example/ucp2/ui/viewmodel/ListMataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/ListMataKuliahViewModel.kt
@@ -7,7 +7,37 @@ import com.example.ucp2.repository.RepositoryMatkul
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 
+class ListMataKuliahViewModel(
+    private val repositoryMatkul: RepositoryMatkul
+) : ViewModel() {
 
+    val mkListUiState: StateFlow<MkListUiState> = repositoryMatkul.getALLMataKuliah()
+        .filterNotNull()
+        .map { mataKuliahList ->
+            MkListUiState(
+                listMk = mataKuliahList.toList(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(MkListUiState(isLoading = true))
+            delay(900)
+        }
+        .catch { exception ->
+            emit(
+                MkListUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = exception.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = MkListUiState(isLoading = true)
+        )
+}
 
 data class MkListUiState(
     val listMk: List<MataKuliah> = listOf(),
