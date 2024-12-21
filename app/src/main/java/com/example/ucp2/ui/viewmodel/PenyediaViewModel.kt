0a5174b115f88c3b4123fb5dc0bff8584a0ad607
mrendy58 <derendyyy58@gmail.com>
2024-12-21 10:31:54 +0700

    membuat PenyediaViewModel untuk mengelola dan menyediakan instance ViewModel

--- app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -1,11 +1,56 @@
 package com.example.ucp2.ui.viewmodel
 
-import android.text.Editable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.KrsApp
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+        // Initializer untuk InsertDsnViewModel
+        initializer {
+            InsertDsnViewModel(
+                repositoryDsn = KrsApp().containerApp.repositoryDsn
+            )
+        }
 
+        // Initializer untuk InsertMkViewModel
+        initializer {
+            InsertMkViewModel(
+                repositoryMatkul = KrsApp().containerApp.repositoryMatkul
+            )
+        }
 
+        // Initializer untuk HomeOptionsViewModel
+        initializer {
+            HomeOptionsViewModel(
+                repositoryDsn = KrsApp().containerApp.repositoryDsn,
+                repositoryMatkul = KrsApp().containerApp.repositoryMatkul
+            )
+        }
+
+        // Initializer untuk DetailMkViewModel dengan SavedStateHandle
+        initializer {
+            val savedStateHandle = createSavedStateHandle()
+            DetailMkViewModel(
+                repositoryMatkul = KrsApp().containerApp.repositoryMatkul,
+                savedStateHandle = savedStateHandle
+            )
+        }
+
+        // Initializer untuk UpdateMkViewModel dengan SavedStateHandle
+        initializer {
+            val savedStateHandle = createSavedStateHandle()
+            UpdateMkViewModel(
+                savedStateHandle = savedStateHandle,
+                repositoryMatkul = KrsApp().containerApp.repositoryMatkul
+            )
+        }
     }
 }
+
+// Ekstensi untuk mendapatkan instance KrsApp dari CreationExtras
+fun CreationExtras.KrsApp(): KrsApp =
+    this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp
